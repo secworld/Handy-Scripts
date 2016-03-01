a6054c79803ffc35eff676dfec338405f2180cd7
VISHNU RAJU <rajuvishnu52@gmail.com>
2016-03-01 10:27:37 +0530

    Disable UAC for remote restrictions remotely
    While doing Nessus scan on workgroup systems, I have noted an issue of UAC blocking nessus audit traffic from reading registry keys. ResitryKeyInsert.ps1 will add LocalAccountTokenFilterPolicy key inoder to disable UAC. After scanning, you can revert back (enable UAC) using the ResitryKeyDelete.ps1 powershell script. Note: Please test the scripts in a test environment before running.

--- ResitryKeyDelete.ps1
+++ ResitryKeyDelete.ps1
@@ -1,5 +1,5 @@
-$servers = “<COMPUTER NAME-1>",”<COMPUTER NAME-2>",”<COMPUTER NAME-3>","<COMPUTER NAME-4>","<COMPUTER NAME-5>"
+$Servers = “<COMPUTER NAME-1>",”<COMPUTER NAME-2>",”<COMPUTER NAME-3>","<COMPUTER NAME-4>","<COMPUTER NAME-5>"
 
 $credential = Get-Credential -Credential Administrator
 
-Invoke-Command -ComputerName $servers -Credential $credential -ScriptBlock {Remove-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\LocalAccountTokenFilterPolicy}
+Invoke-Command -ComputerName $Servers -Credential $credential -ScriptBlock {Remove-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\LocalAccountTokenFilterPolicy}
