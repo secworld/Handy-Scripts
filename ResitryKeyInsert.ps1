0b9d6a5538babdf9d01def30bd0a79512f54852a
VISHNU RAJU <rajuvishnu52@gmail.com>
2016-03-01 10:23:11 +0530

    Update ResitryKeyInsert.ps1

--- ResitryKeyInsert.ps1
+++ ResitryKeyInsert.ps1
@@ -1,5 +1,5 @@
-$servers = “New-IndusDirect-Web-W2K8",”Meridian",”CFDINDUSLOAN","HHTFTP","PROGENIE"
+$servers =  â€œ<COMPUTER NAME-1>",â€<COMPUTER NAME-2>",â€<COMPUTER NAME-3>","<COMPUTER NAME-4>","<COMPUTER NAME-5>"
 
-$credential = Get-Credential -Credential iblvaadmin
+$credential = Get-Credential -Credential Administrator
 
-Invoke-Command -ComputerName $servers -Credential $credential -ScriptBlock {New-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system -Name LocalAccountTokenFilterPolicy -Value “1”}
+Invoke-Command -ComputerName $servers -Credential $credential -ScriptBlock {New-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system -Name LocalAccountTokenFilterPolicy -Value â€œ1â€}
