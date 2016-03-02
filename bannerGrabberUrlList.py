7c654d2d8c3acbc946893bd75dbb5bb43db21f59
VISHNU RAJU <rajuvishnu52@gmail.com>
2016-03-02 12:17:53 +0530

    WebServer Banner Grabber
    WebServer Banner Grabber

--- bannerGrabberUrlList.py
+++ bannerGrabberUrlList.py
@@ -1,6 +1,7 @@
 import sys,socket,httplib
 
-with open('<Path To IP List File>', 'r') as f:
+ipList = raw_input("Enter the full path of the IP list file : ")
+with open(ipList, 'r') as f:
     for ip in f:
             host = ip.rstrip('\n')
             httpurl = str(host)+str(":80")
@@ -8,40 +9,38 @@ with open('<Path To IP List File>', 'r') as f:
             try:
                 try:
                     print httpurl
-                    c = httplib.HTTPConnection(httpurl,timeout=10)
+                    c = httplib.HTTPConnection(httpurl,timeout=2)
                     c.request ("GET", "/")
                     r2 = c.getresponse()
-                    print r2.status
+                    print "Response code : ",r2.status
                     r1 = dict(r2.getheaders())
                     WebServer = r2.getheader("server")
                     ContentType = r2.getheader("content-type")
                     d2 = r2.read()
-                    print "\n\nBanner :"
                     print "Banner Fields :",r1
                     print "\n\nWebServer Name :",WebServer
                     print "Content Type :",ContentType
-                    print "\n\nResponse is : "
-                    print d2
+                    #print "\n\nResponse is : "
+                    #print d2
                     print "\n\n"
                 except:
                     print httpsurl
-                    c = httplib.HTTPConnection(httpsurl,timeout=10)
+                    c = httplib.HTTPConnection(httpsurl,timeout=2)
                     c.request ("GET", "/")
                     r2 = c.getresponse()
-                    print r2.status
+                    print "Response code : ",r2.status
                     r1 = dict(r2.getheaders())
                     WebServer = r2.getheader("server")
                     ContentType = r2.getheader("content-type")
                     d2 = r2.read()
-                    print "\n\nBanner :"
                     print "Banner Fields :",r1
                     print "\n\nWebServer Name :",WebServer
                     print "Content Type :",ContentType
-                    print "\n\nResponse is : "
-                    print d2
+                    #print "\n\nResponse is : "
+                    #print d2
                     print "\n\n"
             except:
-                print "Connection Timed out !!!\nTrying next ..."
+                print "Connection Timed out !!!\nTrying next ...\n"
                 continue
 
 
